ce9904f6c3d78186621662bb978933a3ef473787
PraiseMassa <massapraise@gmail.com>
2026-09-02 10:39:14 +0300

    add render endpoints

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const { PrismaClient } = pkg;
 
 const express = require('express');
 const cors = require('cors');
-const app = express();
+//const app = express();
 const prisma = new PrismaClient();
 
 app.use(cors());
